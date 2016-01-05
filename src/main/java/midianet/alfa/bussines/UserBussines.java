e81567124e147c576407b010afc53438aff3b1f3
marcosfernandocosta <P@$w0rd>
2016-01-05 20:47:02 -0200

    add exception framework, intro persistence spring jdbc impl

--- src/main/java/midianet/alfa/bussines/UserBussines.java
+++ src/main/java/midianet/alfa/bussines/UserBussines.java
@@ -1,13 +1,41 @@
 package midianet.alfa.bussines;
 
+import midianet.alfa.model.User;
 import midianet.alfa.repository.UserRepository;
+import midianet.exception.BussinesException;
+import midianet.exception.InfraException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class UserBussines {
 
     @Autowired
     private UserRepository repository;
+
+
+    public User save(final User user) throws BussinesException, InfraException {
+        if(user.getId() == null){
+            repository.add(user);
+        }else{
+            repository.update(user);
+        }
+        return user;
+    }
+
+    public void delete(final User user )throws  BussinesException,InfraException{
+        repository.delete(user);
+    }
+
+    public User findById(final long id) throws  BussinesException,InfraException {
+        return repository.findById(id);
+    }
+
+    public List<User> listAll() throws InfraException{
+        return repository.listAll();
+    }
 
 }
--- src/main/java/midianet/alfa/repository/UserRepository.java
+++ src/main/java/midianet/alfa/repository/UserRepository.java
@@ -1,8 +1,38 @@
 package midianet.alfa.repository;
 
+import midianet.alfa.model.User;
+import midianet.exception.InfraException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class UserRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public User add (final User user) throws InfraException{
+        return user;
+    }
+
+    public User update(final User user) throws InfraException{
+        return user;
+    }
+
+    public void delete(final User user )throws InfraException{
+
+    }
+
+    public User findById(final long id) throws InfraException {
+        return new User();
+    }
+
+    public List<User> listAll() throws InfraException{
+        return new ArrayList<User>();
+    }
 
 }
